77942936d014dd10567b3155e9cd2cb775614b18
Rodrigo <rodrigo.m.esteves06@gmail.com>
2025-06-17 16:18:06 +0100

    melhora no botao do xml com js

--- src/script.js
+++ src/script.js
@@ -1,16 +1,16 @@
-  const changeBtn = document.getElementById("change-text-btn"); //depois temos de mudar para nome real do elemento
-  const textToChange = document.getElementById("text-to-change"); //depois temos de mudar para nome real do elemento
-  if (changeBtn && textToChange) {
-    changeBtn.addEventListener("click", () => {
-      textToChange.textContent = "XML Carregado!";
-    });
-  }
+const changeBtn = document.getElementById("carregar-escolas-btn");
 
-  const hoverTarget = document.getElementById("hover-target"); //depois temos de mudar para nome real do botão
+if (changeBtn) {
+  changeBtn.addEventListener("click", () => {
+    changeBtn.textContent = "XML Carregado!";
+  });
+}
+
+  const hoverTarget = document.getElementById("carregar-escolas-btn");
 
   if (hoverTarget) {
     hoverTarget.addEventListener("mouseover", () => {
-      hoverTarget.style.backgroundColor = "#ffeb3b"; //exemplo aleatório de cor
+      hoverTarget.style.backgroundColor = "#ffeb3b";
     });
 
     hoverTarget.addEventListener("mouseout", () => {
